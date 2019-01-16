271df0dd119a5aae4f9f589395932d89872f7944
joma74 <dev-mails@gmx.net>
2019-01-16 23:59:43 +0100

    feat(setup): show debug-grid only if ENV_NODE=development

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   plugins: [
     require("postcss-import"),
+    require("postcss-advanced-variables")({
+      variables: { cssenv: process.env.NODE_ENV },
+    }),
     require("tailwindcss")("./tailwind.js"),
     require("autoprefixer"),
   ],
