8f9ff1dd6e82c1491e6b69043a742d31b26910ed
joma74 <dev-mails@gmx.net>
2019-02-05 10:08:45 +0100

    setup(dep-mgt): upgrade vue and add ccs-advanced

--- src/tests/index-test.js
+++ src/tests/index-test.js
@@ -1,5 +1,4 @@
 import { Selector, ClientFunction } from "testcafe"
-import pretty from "pretty"
 import identifyUserAgent from "./utils/identify-useragent"
 
 const fixtureName = "Index_Page_Test"
@@ -22,43 +21,4 @@ test(testName, async (t) => {
       "/" +
       "scsh_1.png",
   )
-
-  const heading = await Selector("h1[data-desc='heading']")
-
-  let headingText = await heading.innerText
-
-  await t.expect(headingText).eql("Greetings hot-updateable tailwindcss")
-
-  const svgDashboard = await Selector("svg[data-desc='dashboard']")
-
-  await t.expect(svgDashboard.visible).ok()
-
-  const subheadline = await Selector("p[data-desc='subheadline']")
-
-  const subheadlineValue = await t.eval(() => subheadline().innerHTML, {
-    dependencies: { subheadline },
-  })
-
-  const expectedSubHeadlineValue = pretty(`
-  	<svg data-desc="dashboard" class="w-6 h-6 opacity-50 fill-current relative mr-2">
-  		<use href="src/assets/svg/toolbar.svg#dashboard"></use>
-	</svg>
-	Powered by
-	<svg data-desc="cog" class="w-6 h-6 opacity-50 fill-current relative ml-2">
-		<use href="src/assets/svg/toolbar.svg#cog"></use>
-	</svg>`)
-
-  await t.expect(pretty(subheadlineValue)).eql(expectedSubHeadlineValue)
-
-  const svgCog = await Selector("svg[data-desc='cog']")
-
-  await t.expect(svgCog.visible).ok()
-
-  const imageWebpack = await Selector("img[data-desc='webpack']")
-
-  await t.expect(imageWebpack.visible).ok()
-
-  const imageVue = await Selector("img[data-desc='vue']")
-
-  await t.expect(imageVue.visible).ok()
 })
